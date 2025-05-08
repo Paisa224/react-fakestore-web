65132e285fe52225e01238950c14d9b9f9ba9487
Paisa224 <manuelsalinas652@gmail.com>
2025-05-08 16:52:42 -0300

    chore(env): implement environment variable support and add .env.example

--- src/api/fakestore.ts
+++ src/api/fakestore.ts
@@ -1,24 +1,24 @@
-const BASE_URL = 'https://fakestoreapi.com'
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export const login = async (username: string, password: string) => {
   const res = await fetch(`${BASE_URL}/auth/login`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ username, password }),
-  })
+  });
 
-  if (!res.ok) throw new Error('Credenciales inválidas')
-  return res.json()
-}
+  if (!res.ok) throw new Error('Credenciales inválidas');
+  return res.json();
+};
 
 export const getProducts = async () => {
-  const res = await fetch(`${BASE_URL}/products`)
-  if (!res.ok) throw new Error('Error al cargar productos')
-  return res.json()
-}
+  const res = await fetch(`${BASE_URL}/products`);
+  if (!res.ok) throw new Error('Error al cargar productos');
+  return res.json();
+};
 
 export const getProductById = async (id: number) => {
-  const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+  const res = await fetch(`${BASE_URL}/products/${id}`);
   if (!res.ok) throw new Error('No se pudo cargar el producto');
   return res.json();
 };
